9bf4d276d0b0cc0b17302470178a88dd8428d2f9
Mohsen Jalali <mniroomandj@gmail.com>
2022-11-16 17:04:25 +0330

    feat: create media widget

---
+++ lib/src/widgets/media_widget.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_media_picker/src/models/media_model.dart';
+
+class MediaWidget extends StatelessWidget {
+  /// Required params which will be handled in package
+  final MediaModel media;
+  final Function() onSelect;
+
+  /// Ui customization props
+  final BorderRadius? borderRadius;
+
+  final BoxShape? boxShape;
+
+  final List<BoxShadow>? boxShadow;
+
+  final BoxFit? mediaFit;
+
+  final Border? mediaBorder;
+
+  final Color? mediaBackgroundColor;
+
+  final Color? mediaSkeletonBaseColor;
+
+  final Color? mediaSkeletonShimmerColor;
+
+  final Widget? loadingWidget;
+
+  const MediaWidget({
+    Key? key,
+    required this.onSelect,
+    required this.media,
+    this.borderRadius,
+    this.boxShadow,
+    this.boxShape,
+    this.mediaFit,
+    this.mediaBorder,
+    this.mediaBackgroundColor,
+    this.mediaSkeletonBaseColor,
+    this.mediaSkeletonShimmerColor,
+    this.loadingWidget,
+  })  : assert(borderRadius != null && boxShape != BoxShape.circle),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onSelect,
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: mediaBackgroundColor,
+          borderRadius: borderRadius,
+          shape: boxShape ?? BoxShape.rectangle,
+          border: mediaBorder,
+          boxShadow: boxShadow,
+        ),
+        child: ValueListenableBuilder<MediaState>(
+          valueListenable: media.mediaState,
+          builder: (context, mediaState, child) {
+            switch (mediaState) {
+              case MediaState.success:
+                return Image.memory(
+                  media.image!,
+                  fit: mediaFit ?? BoxFit.fill,
+                );
+              default:
+                return loadingWidget ?? const SizedBox();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
