7942c32958fe7ac804ff47baaf6414216d80834c
Mohsen <mniroomandj@gmail.com>
2022-12-07 15:30:57 +0330

    fix: rename export path

--- lib/flutter_media_picker.dart
+++ lib/flutter_media_picker.dart
@@ -1,7 +1,7 @@
 library flutter_media_picker;
 
 
-export 'src/main.dart';
+export 'src/media_picker_bottom_sheet.dart';
 
 export 'src/models/cropper_model.dart';
 export 'src/models/modal_header_model.dart';
