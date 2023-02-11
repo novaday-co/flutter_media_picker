a2727530ddc04c10c2ecf3a0e7fccb279d296257
Amir Jabbari <am.jabbarii@gmail.com>
2023-02-11 13:13:55 +0330

    feat: add image extension to picked media model

--- lib/src/models/picked_media.dart
+++ lib/src/models/picked_media.dart
@@ -3,9 +3,11 @@ import 'package:flutter/services.dart';
 class PickedMedia {
   String? path;
   Uint8List? bytes;
+  String? extension;
 
   PickedMedia({
     this.path,
     this.bytes,
+    this.extension,
   });
 }
