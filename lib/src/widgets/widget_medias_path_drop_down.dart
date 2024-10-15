89b4b85511580c9e092ac13a7f6fd6b4188c6d05
javad ansari <jansari.it@gmail.com>
2024-10-15 13:27:59 +0330

    Applying breaking changes to the dropDownButton2 library

--- lib/src/widgets/widget_medias_path_drop_down.dart
+++ lib/src/widgets/widget_medias_path_drop_down.dart
@@ -15,7 +15,7 @@ class MediaPickerDropDown extends StatelessWidget {
   final Color? dropDownButtonBackgroundColor;
 
   const MediaPickerDropDown({
-    Key? key,
+    super.key,
     required this.items,
     required this.onChanged,
     required this.selectedItemIndex,
@@ -26,13 +26,14 @@ class MediaPickerDropDown extends StatelessWidget {
     this.dropDownItemsTextStyle,
     this.selectedItemBackgroundColor,
     this.dropDownItemsBackgroundColor,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
     return DropdownButtonHideUnderline(
-      child: DropdownButtonFormField2(
-        icon: const SizedBox(),
+      child: DropdownButtonFormField2<int>(
+        //icon: const SizedBox(),
+        iconStyleData: const IconStyleData(icon: SizedBox()),
         customButton: Padding(
           padding: const EdgeInsets.only(bottom: 4.0),
           child: Row(
@@ -44,12 +45,13 @@ class MediaPickerDropDown extends StatelessWidget {
                   items[selectedItemIndex].toString(),
                   overflow: TextOverflow.ellipsis,
                   maxLines: 1,
-                  style: dropDownButtonTextStyle ?? const TextStyle(fontSize: 18),
+                  style: dropDownButtonTextStyle ??
+                      const TextStyle(
+                        fontSize: 18,
+                      ),
                 ),
               ),
-              const SizedBox(
-                width: 8,
-              ),
+              const SizedBox(width: 8),
               const Icon(
                 Icons.arrow_drop_down,
                 color: Colors.grey,
@@ -60,9 +62,11 @@ class MediaPickerDropDown extends StatelessWidget {
         ),
         decoration: InputDecoration(
           fillColor: dropDownButtonBackgroundColor,
-          filled: dropDownButtonBackgroundColor != null ? true:false,
-          contentPadding:
-              const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+          filled: dropDownButtonBackgroundColor != null ? true : false,
+          contentPadding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 16,
+          ),
           constraints: const BoxConstraints(),
           hintText: hintText,
           hintStyle: const TextStyle(fontSize: 18),
@@ -82,32 +86,44 @@ class MediaPickerDropDown extends StatelessWidget {
             borderSide: BorderSide.none,
           ),
         ),
-        dropdownOverButton: true,
+        //dropdownOverButton: true, //jansari added: there is no replacement in the new version
         // customItemsHeight: 36,
-        items: items
-            .map((item) => DropdownMenuItem(
-                  value: items.indexOf(item),
-                  child: Text(
-                    item.toString(),
-                    style:
-                        dropDownItemsTextStyle ?? const TextStyle(fontSize: 16),
-                  ),
-                ))
-            .toList(),
+        items: items.map((item) {
+          return DropdownItem(
+            value: items.indexOf(item),
+            child: Text(
+              item.toString(),
+              style: dropDownItemsTextStyle ?? const TextStyle(fontSize: 16),
+            ),
+          );
+        }).toList(),
         onChanged: onChanged,
         isExpanded: false,
-        selectedItemHighlightColor: selectedItemBackgroundColor ?? Colors.blue,
-        buttonHeight: 45,
-        buttonWidth: 250,
-        value: selectedItemIndex != -1 ? selectedItemIndex : null,
-        itemPadding: const EdgeInsets.symmetric(horizontal: 8),
-        dropdownWidth: width ?? 260,
-        dropdownMaxHeight: 350,
-        offset: const Offset(0, -30),
-        dropdownDecoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: dropDownItemsBackgroundColor,
+        buttonStyleData: const ButtonStyleData(height: 45, width: 250),
+        dropdownStyleData: DropdownStyleData(
+          width: width ?? 260,
+          maxHeight: 350,
+          offset: const Offset(0, -30),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: dropDownItemsBackgroundColor,
+          ),
         ),
+        menuItemStyleData: const MenuItemStyleData(
+          padding: EdgeInsets.symmetric(horizontal: 8),
+        ),
+        // selectedItemHighlightColor: selectedItemBackgroundColor ?? Colors.blue, //jansari added: there is no replacement in the new version
+        // value: selectedItemIndex != -1 ? selectedItemIndex : null, //jansari added: there is no replacement in the new version
+        // buttonHeight: 45,
+        // buttonWidth: 250,
+        // itemPadding: const EdgeInsets.symmetric(horizontal: 8),
+        // dropdownWidth: width ?? 260,
+        // dropdownMaxHeight: 350,
+        // offset: const Offset(0, -30),
+        // dropdownDecoration: BoxDecoration(
+        //   borderRadius: BorderRadius.circular(10),
+        //   color: dropDownItemsBackgroundColor,
+        // ),
       ),
     );
   }
